77fbe8ef8dd5a47d7a33b73d62cd679dbfcc5529
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-09-27 15:36:01 +0300

    navigation skeleton

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,47 @@ enum KEY_BINDING {
   PAGE_NEXT = 'c',
 }
 
+let selectedContentEntry = null;
+
 // Function to handle keypress events
 function handleKeyPress(event: KeyboardEvent) {
-  // TODO: ignore if modified (Ctrl/Cmd/Alt/Opt), we don't want to react on Ctrl+s / Ctrl+c
-  console.log(`Key pressed: ${event.key}`);
+  console.log(`Key pressed: ${event.key}`, event);
+
+  switch (event.key) {
+    case KEY_BINDING.CONTENT_PREVIOUS:
+      handleContextPrevious();
+      break;
+    //...
+  }
+}
+
+function handleContextPrevious() {
+  // reset .selectedContentEntry class on selectedContentEntry
+
+  // IF selectedContentEntry SET selectedContentEntry to the next Content Entry
+  // ELSE SET selectedContentEntry to the first Content Entry
+
+  // set .selectedContentEntry class on the new selectedContentEntry
+
+  // if the contained image is not fully visible due to overflow - expand post
+
+  // scroll the .selectedContentEntry to the top of the page
+}
+
+function handleContextNext() {
+
+}
+
+function handlePostPrevious() {
+
+}
+
+function handlePostNext() {
+
+}
+
+function handlePageNext() {
+
 }
 
 // Add event listener for keypress events
