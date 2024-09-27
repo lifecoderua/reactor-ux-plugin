cc6e61d76aba87e1faca7d236f0fe73c28acf356
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-09-27 13:46:02 +0300

    non-functional, builder required

--- src/index.js
+++
@@ -1,2 +0,0 @@
-console.log('ReactorUX loaded');
-document.body.style.border = "5px solid red";
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+console.log('ReactorUX loaded');
+document.body.style.border = "5px solid red";
+
+// TODO: support caps
+// TODO: support cyrillic / charkey
+enum KEY_BINDING {
+  CONTENT_PREVIOUS = 'w',
+  CONTENT_NEXT = 's',
+  POST_PREVIOUS = 'a',
+  POST_NEXT = 'd',
+  PAGE_NEXT = 'c',
+}
+
+// Function to handle keypress events
+function handleKeyPress(event) {
+  // TODO: ignore if modified (Ctrl/Cmd/Alt/Opt), we don't want to react on Ctrl+s / Ctrl+c
+  console.log(`Key pressed: ${event.key}`);
+}
+
+// Add event listener for keypress events
+document.addEventListener('keypress', handleKeyPress);
